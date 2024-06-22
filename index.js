486730072f6bef8b468743e85bceb4731a634949
sujith <123747958+grzzlyb@users.noreply.github.com>
2024-06-22 16:04:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 // Configure our HTTP server to respond with "Hello, World!" to all requests
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello! This is a sample node server. Testing Jenkins Job Build 1... \nTesting twooo!!!');
+  response.end('Hello, This build was taken by Daniel.');
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
